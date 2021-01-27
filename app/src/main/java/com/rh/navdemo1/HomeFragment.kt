d9fbb205b291b8d3524a693efe03ce75795490a3
rhaldar <rhaldar@gmail.com>
2021-01-27 13:10:03 +0530

    Demo Navigation

---
+++ app/src/main/java/com/rh/navdemo1/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.rh.navdemo1
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.rh.navdemo1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       // return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+
+        binding.button.setOnClickListener {
+            if (!TextUtils.isEmpty(binding.editTextTextPersonName.text.toString())){
+
+                val bundle : Bundle = bundleOf("user_input" to binding.editTextTextPersonName.text.toString())
+
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+
+            }else{
+                Toast.makeText(activity, "Please insert", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/rh/navdemo1/SecondFragment.kt
@@ -0,0 +1,29 @@
+package com.rh.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.rh.navdemo1.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding : FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        var input : String? = arguments?.getString("user_input")
+        binding.textView.text = input.toString()
+        return binding.root
+
+    }
+
+}
